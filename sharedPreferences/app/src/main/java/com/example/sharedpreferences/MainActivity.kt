4aa85e2d0d321600b2d94800ad77ef947475be98
Neslisah <neslisah.celek@outlook.com>
2023-03-30 17:11:54 +0300

    Add result text view and shared preferences

--- sharedPreferences/app/src/main/java/com/example/sharedpreferences/MainActivity.kt
+++ sharedPreferences/app/src/main/java/com/example/sharedpreferences/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.sharedpreferences
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var sharedPreferences:SharedPreferences
+    var number1:Int? = null
+    var number2:Int?=null
+    var result:Int?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sharedPreferences = this.getSharedPreferences(
+            "com.example.sharedpreferences",Context.MODE_PRIVATE)
+    }
+    fun sum(view:View){
+        number1=findViewById<TextView>(R.id.editTextNumber1).text.toString().toInt()
+        number2=findViewById<TextView>(R.id.editTextNumber2).text.toString().toInt()
+        if (number1== null||number2 == null){
+            findViewById<TextView>(R.id.resultText).text = "Please enter a valid number."
+        }else {
+            result = number1!! + number2!!
+            sharedPreferences.edit().putInt("result",result!!).apply()
+            findViewById<TextView>(R.id.resultText).text = "Result is: $result"
+        }
+    }
+    fun sub(view:View){
+        number1=findViewById<TextView>(R.id.editTextNumber1).text.toString().toInt()
+        number2=findViewById<TextView>(R.id.editTextNumber2).text.toString().toInt()
+        if (number1== null||number2 == null){
+            findViewById<TextView>(R.id.resultText).text = "Please enter a valid number."
+        }else {
+            result = number1!! - number2!!
+            sharedPreferences.edit().putInt("result",result!!).apply()
+            findViewById<TextView>(R.id.resultText).text = "Result is: $result"
+        }
+    }
+    fun mul(view:View){
+        number1=findViewById<TextView>(R.id.editTextNumber1).text.toString().toInt()
+        number2=findViewById<TextView>(R.id.editTextNumber2).text.toString().toInt()
+        if (number1== null||number2 == null){
+            findViewById<TextView>(R.id.resultText).text = "Please enter a valid number."
+        }else {
+            result = number1!! * number2!!
+            sharedPreferences.edit().putInt("result",result!!).apply()
+            findViewById<TextView>(R.id.resultText).text = "Result is: $result"
+        }
+    }
+    fun div(view:View){
+        number1=findViewById<TextView>(R.id.editTextNumber1).text.toString().toInt()
+        number2=findViewById<TextView>(R.id.editTextNumber2).text.toString().toInt()
+        if (number1== null||number2 == null){
+            findViewById<TextView>(R.id.resultText).text = "Please enter a valid number."
+        }else {
+            result = number1!! / number2!!
+            sharedPreferences.edit().putInt("result",result!!).apply()
+            findViewById<TextView>(R.id.resultText).text = "Result is: $result"
+        }
     }
 }
