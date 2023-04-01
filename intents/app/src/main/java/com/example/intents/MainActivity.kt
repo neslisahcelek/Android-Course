62156f92dd19b8f214b66669e90ff8608a83e02a
Neslisah <neslisah.celek@outlook.com>
2023-04-01 10:40:20 +0300

    Fix bugs

--- intents/app/src/main/java/com/example/intents/MainActivity.kt
+++ intents/app/src/main/java/com/example/intents/MainActivity.kt
@@ -16,34 +16,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //nextAlert()
-
     }
-     fun next(view:View){
-        if (nextAlert()==true){
+     fun next(){
             val intent = Intent(applicationContext,SecondActivity::class.java)
             intent.putExtra("name",binding.editTextName.text.toString())
             intent.putExtra("username",binding.editTextNickname.text.toString())
             startActivity(intent)
             finish()
-        }else{
-
-        }
     }
-    fun nextAlert():Boolean{
+    fun nextAlert(view:View){
         val alert = AlertDialog.Builder(this)
-        var result = true
         alert.setTitle("Save")
         alert.setMessage("Are you sure?")
         alert.setPositiveButton("Yes") {dialog, which ->
             Toast.makeText(applicationContext,"Saved!",Toast.LENGTH_LONG).show()
+            next()
         }
         alert.setNegativeButton("No") {dialog, which ->
-            Toast.makeText(applicationContext,"Not Saved!",Toast.LENGTH_LONG)
-            result=false
+            Toast.makeText(applicationContext,"Not Saved :(",Toast.LENGTH_LONG).show()
         }
         alert.show()
-        return result
     }
-
 }
