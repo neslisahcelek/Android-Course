23ec52e5293aefe80267ee75c71433f9d9cf6278
Neslisah <neslisah.celek@outlook.com>
2023-04-02 15:49:13 +0300

    Add view binding

--- Runnables/app/src/main/java/com/example/runnables/MainActivity.kt
+++ Runnables/app/src/main/java/com/example/runnables/MainActivity.kt
@@ -3,16 +3,20 @@ package com.example.runnables
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.runnables.databinding.ActivityMainBinding
 
-var number =0
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var number=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-
     fun start(view: View){
-
+        number+=1
+        binding.textViewNumber.text="$number"
     }
     fun stop(view:View){
 
