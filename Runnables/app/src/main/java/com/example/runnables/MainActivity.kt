9905d4ac875fd2be56db81020781df4078ceec01
Neslisah <neslisah.celek@outlook.com>
2023-04-02 16:55:01 +0300

    Add stop,start,restart buttons

--- Runnables/app/src/main/java/com/example/runnables/MainActivity.kt
+++ Runnables/app/src/main/java/com/example/runnables/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.runnables
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import com.example.runnables.databinding.ActivityMainBinding
 
@@ -9,16 +10,33 @@ import com.example.runnables.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var number=0
+    var runnable:Runnable = Runnable{} //tekrarlama için
+    var handler:Handler = Handler() //süre ayarlama için
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
     fun start(view: View){
-        number+=1
-        binding.textViewNumber.text="$number"
+        runnable = object : Runnable{
+            override fun run() {
+                number+=1
+                binding.textViewNumber.text="$number"
+
+                handler.postDelayed(this,1000)
+                binding.buttonStart.setEnabled(false)
+            }
+        }
+        handler.post(runnable)
     }
     fun stop(view:View){
-
+        handler.removeCallbacks(runnable)
+        binding.textViewNumber.text="$number"
     }
+    fun restart(view: View){
+        handler.removeCallbacks(runnable)
+        number=0
+        start(view)
+    }
+
 }
