c0415806563799a0fa5754837f1a9d63a0f2892e
Neslisah <neslisah.celek@outlook.com>
2023-04-02 15:44:04 +0300

    Create project

---
+++ Runnables/app/src/main/java/com/example/runnables/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.runnables
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+var number =0
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun start(view: View){
+
+    }
+    fun stop(view:View){
+
+    }
+}
